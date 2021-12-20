216088e2a4ced4bb7b191b7926940f0bab49d75a
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-20 10:54:31 +0300

    intial commit: basic structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors")
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`server on port ${PORT}`);
+})
