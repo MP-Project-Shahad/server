c665b222a030cf1a872e498df061c6d3a96a0305
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-20 13:43:53 +0300

    finished comment controller

--- db/modules/comments.js
+++ db/modules/comments.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const comment = new mongoose.Schema({
   desc: { type: String, required: true },
+  img: { type: String },
   timeStamp: { type: Date },
   isDel: { type: Boolean, default: false },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
